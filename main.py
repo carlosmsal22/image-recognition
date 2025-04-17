e43208099971d69c9baee19a406a5b2233903237
carlosmsal22 <CARLOSMSAL22@GMAIL.COM>
2025-04-17 10:32:30 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-main.py
+import numpy as np 
+import matplotlib.pyplot as plt 
+import os 
+import cv2 from keras.applications.resnet50 
+import ResNet50 from keras.preprocessing 
+import image from keras.applications.resnet50 
+import preprocess_input, decode_predictions
